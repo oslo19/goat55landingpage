668fe572f6510850c020f0409fc4d24d43d4c348
Genard Rey Zozobrado <zozobradogenardrey@gmail.com>
2024-11-02 17:28:36 +0800

    I added social media icons and testimonials

--- components/Header.php
+++ components/Header.php
@@ -1,7 +1,7 @@
 <div>
   <header class="rounded-lg m-4">
     <div class="w-full mx-auto max-w-screen-xl p-4 flex items-center justify-between">
-      <img src="/img/logo.png" alt="Logo" class="rounded-full w-24 h-24 border-4 border-blue-500">
+      <img src="img/logo.png" alt="Logo" class="rounded-full w-24 h-24 border-4 border-blue-500">
       <span class="text-sm text-black font-bold md:text-lg text-center">
        Need help? Submit a question via webchat or <br> email goat55marketing@gmail.com
       </span>
